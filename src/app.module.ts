4b5923bba206f3488d031a70a36fbe6efa5cf7a7
Henrique Aquino <henrique.aquino-extern@renault.com>
2023-08-02 17:24:01 -0300

    02/08

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UsuarioModule } from './usuario/usuario.module';
 import { ProdutoModule } from './produto/produto.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfigService } from './config/postgres.config.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsuarioModule, ProdutoModule],
+  imports: [
+    UsuarioModule,
+    ProdutoModule,
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
